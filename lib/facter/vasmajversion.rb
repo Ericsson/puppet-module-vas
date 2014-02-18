d212493fe04041c285a918d7914076b35e54d565
Johan Wennerberg <johan.x.wennerberg@ericsson.com>
2014-02-18 17:05:48 +0100

    Add vasmajversion fact as separate file
    This solves a name space issue with legacy module eis_vas.

--- lib/facter/vasmajversion.rb
+++ lib/facter/vasmajversion.rb
@@ -1,12 +1,12 @@
-# vas_version
+# vasmajversion
 
-Facter.add('vas_version') do
+Facter.add('vasmajversion') do
   setcode do
     test_installed = 'test -f /opt/quest/bin/vastool; echo $?'
     if Facter::Util::Resolution.exec(test_installed) == '0'
       cmd = '/opt/quest/bin/vastool -v | grep "^vastool"'
       response = Facter::Util::Resolution.exec(cmd)
-      response.split(" ")[3]
+      response.split(" ")[3].chars.first
     end
   end
 end
