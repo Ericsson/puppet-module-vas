e38f6b2588d773aee537e4ecf1982301146a93b1
Phil Friderici <phil.friderici@i-tee.de>
2015-12-04 13:07:52 +0000

    Refactor hiera related spec test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1093,58 +1093,49 @@ DOMAIN\\adgroup:group::
 
   end
 
-  describe 'hiera merge parameter' do
-
-    hiera_merge_parameters = {
-      'user_override_entries' =>
-        {
-          :mergeparameter => 'user_override_hiera_merge',
-          :mergetitle     => 'vas_user_override',
-        },
-      'group_override_entries' =>
-        {
-          :mergeparameter => 'group_override_hiera_merge',
-          :mergetitle     => 'vas_group_override',
-        },
-    }
-
-    let :facts do
+  hiera_merge_parameters = {
+    'user_override_hiera_merge' =>
       {
-        :kernel                     => 'Linux',
-        :osfamily                   => 'RedHat',
-        :lsbmajdistrelease          => '6',
-        :operatingsystemmajrelease  => '6',
-        :fqdn                       => 'hieramerge.example.local',
-        :vas_version                => '4.1.0.21518',
-        :spectest                   => 'hieramerge',
-      }
-    end
+        :filename      => 'vas_user_override',
+        :true_content  => "user2@example.com:1235:12346::::/bin/bash\nuser@example.com:1234:12345::::/bin/ksh\n",
+        :false_content => "user2@example.com:1235:12346::::/bin/bash\n",
+      },
+    'group_override_hiera_merge' =>
+      {
+        :filename      => 'vas_group_override',
+        :true_content  => "EXAMPLE\\Engineering_Group:enggrp::\nEXAMPLE\\dbadmins:::localuser,bob\n",
+        :false_content => "EXAMPLE\\Engineering_Group:enggrp::\n",
+      },
+  }
 
-    hiera_merge_parameters.each do |parameter,v|
+  file_header = "# This file is being maintained by Puppet\.\n# DO NOT EDIT\n"
 
-      context "#{parameter} set to true" do
-        let :params do
-          {
-            :"#{v[:mergeparameter]}" => true,
-          }
-        end
-        truefixture = File.read(fixtures("files/hieramerge-#{parameter}.true"))
-        it { should contain_file("#{v[:mergetitle]}").with_content(truefixture) }
+  hiera_merge_parameters.each do |parameter,v|
+    describe 'hiera merge parameters' do
+      let :facts do
+        {
+          :kernel                     => 'Linux',
+          :osfamily                   => 'RedHat',
+          :lsbmajdistrelease          => '6',
+          :operatingsystemmajrelease  => '6',
+          :fqdn                       => 'hieramerge.example.local',
+          :vas_version                => '4.1.0.21518',
+          :parameter_tests            => "#{parameter}",
+        }
       end
 
-      context "#{parameter} set to false" do
-        let :params do
-          {
-            :"#{v[:mergeparameter]}" => false,
-          }
+      [true,false,'true','false'].each do |value|
+        context "when #{parameter} is set to #{value} (as #{value.class})" do
+          let :params do
+            {
+              :"#{parameter}" => value,
+            }
+          end
+          content = "#{Regexp.escape(file_header)}#{Regexp.escape(v[:"#{value}_content"])}"
+          it { should contain_file("#{v[:filename]}").with_content(/^#{content}/) }
         end
-
-        falsefixture = File.read(fixtures("files/hieramerge-#{parameter}.false"))
-        it { should contain_file("#{v[:mergetitle]}").with_content(falsefixture) }
       end
-
     end
-
   end
 
   describe "other" do
@@ -1334,4 +1325,52 @@ DOMAIN\\adgroup:group::
     end
 
   end
+
+  describe 'variable type and content validations' do
+    # set needed custom facts and variables
+    let(:facts) { {
+      :kernel                     => 'Linux',
+      :osfamily                   => 'RedHat',
+      :lsbmajdistrelease          => '6',
+      :operatingsystemmajrelease  => '6',
+      :fqdn                       => 'hieramerge.example.local',
+      :vas_version                => '4.1.0.21518',
+    } }
+    let(:validation_params) { {
+#      :param => 'value',
+    } }
+
+    validations = {
+      'boolean' => {
+        :name    => ['user_override_hiera_merge','group_override_hiera_merge'],
+        :valid   => [true,false,'true','false'],
+        :invalid => ['string',['array'],a={'ha'=>'sh'},3,2.42,nil],
+        :message => '(is not a boolean|Unknown type of boolean)',
+      },
+    }
+
+    validations.sort.each do |type,var|
+      var[:name].each do |var_name|
+
+        var[:valid].each do |valid|
+          context "with #{var_name} (#{type}) set to valid #{valid} (as #{valid.class})" do
+            let(:params) { validation_params.merge({:"#{var_name}" => valid, }) }
+            it { should compile }
+          end
+        end
+
+        var[:invalid].each do |invalid|
+          context "with #{var_name} (#{type}) set to invalid #{invalid} (as #{invalid.class})" do
+            let(:params) { validation_params.merge({:"#{var_name}" => invalid, }) }
+            it 'should fail' do
+              expect {
+                should contain_class(subject)
+              }.to raise_error(Puppet::Error,/#{var[:message]}/)
+            end
+          end
+        end
+
+      end # var[:name].each
+    end # validations.sort.each
+  end # describe 'variable type and content validations'
 end
