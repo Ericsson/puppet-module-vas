8898163d06054f6de86b69790635033bda79d0a3
Phil Friderici <phil.friderici@ericsson.com>
2016-11-08 12:33:12 +0100

    Support Puppet up to 4.8 and Ruby 2.3.1
    - Align testing harness

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,15 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |config|
   config.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
+  config.before :each do
+    # Ensure that we don't accidentally cache facts and environment between
+    # test cases.  This requires each example group to explicitly load the
+    # facts being exercised with something like
+    # Facter.collection.loader.load(:ipaddress)
+    Facter.clear
+    Facter.clear_messages
+  end
+  config.default_facts = {
+    :parameter_tests => nil, # used in hiera, mandatory on Puppet 4.0 to 4.2
+  }
 end
