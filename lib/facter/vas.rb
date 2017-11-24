5e9f857cae3dd4d389b692e8322a379106e024fd
Pål <pal.sveningson@ericsson.com>
2017-11-24 15:45:03 +0100

    Changed some special chars in facts

--- lib/facter/vas.rb
+++ lib/facter/vas.rb
@@ -2,7 +2,7 @@
 Facter.add('vas_usersallow') do
   setcode do
     if File.exists?('/etc/opt/quest/vas/users.allow')
-      cmd = 'cat /etc/opt/quest/vas/users.allow 2>/dev/null | tr -d "\r" | sed '/^$/d' | egrep -v "^#" | sort | tr -s "\n" " "'
+      cmd = 'cat /etc/opt/quest/vas/users.allow 2>/dev/null | tr -d "\r" | sed "/^$/d" | egrep -v "^#" | sort | tr -s "\n" " "'
       Facter::Util::Resolution.exec(cmd)
     end
   end
