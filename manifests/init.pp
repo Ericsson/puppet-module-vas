502e96ebd407c2127cecc1839e5d603f9e5c66fe
Phil Friderici <phil.friderici@elits.com>
2022-05-19 12:47:50 +0000

    Move instructions to inline Puppet Strings

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,491 @@
-# == Class: vas
+# Puppet module to manage DELL Authentication Services previously known as VAS or
+# Quest Authentication Services.
 #
-# Puppet module to manage VAS - Quest Authentication Services
+# When using the users.allow functionality in VAS, make sure to set the following option:
+# pam::allowed_users:
+#   - 'ALL'
+#
+# The module creates facts as below:
+# - vas_usersallow - A list of entries in /etc/opt/quest/vas/users.allow.
+# - vas_domain - The domain that the host belongs to.
+# - vas_server_type - The server types (GC, DC, PDC).
+# - vas_servers - List of servers that VAS is using for authentication.
+# - vas_site - The AD-site that the host belongs to.
+# - vas_version - The complete version-string for the vas-client.
+# - vasmajversion - The Major version of the vas-client.
+#
+# @summary Manages Dell Authentication Services previously known as VAS / QAS.
+#
+# @example Example hiera config
+#   vas::username: 'joinuser'
+#   vas::keytab_source: '/net/server/join.keytab'
+#   vas::computers_ou: 'ou=computers,dc=example,dc=com'
+#   vas::users_ou: 'ou=users,dc=example,dc=com'
+#   vas::nismaps_ou: 'ou=nismaps,dc=example,dc=com'
+#   vas::realm: 'realm.example.com'
+#
+#
+# @param manage_nis
+#   FIXME Missing description
+#
+# @param package_version
+#   The VAS package version. Used when upgrading.
+#
+# @param enable_group_policies
+#   Boolean to control if vas should manage group policies. Manages the vasgp
+#   package. Version is controlled by package_version.
+#
+# @param users_allow_entries
+#  List of users.allow entries. All users are allowed by default.
+#
+# @param users_allow_hiera_merge
+#   Boolean to control merges of all found instances of vas::users_allow_entries
+#   in Hiera. This is useful for specifying users.allow entries at different
+#   levels of the hierarchy and having them all included in the catalog.
+#   This will default to 'true' in future versions.
+#
+# @param users_deny_entries
+#   List of users.deny entries. No users are denied by default.
+#
+# @param users_deny_hiera_merge
+#   Boolean to control merges of all found instances of vas::users_deny_entries
+#   in Hiera. This is useful for specifying users.deny entries at different
+#   levels of the hierarchy and having them all included in the catalog.
+#   This will default to 'true' in future versions.
+#
+# @param user_override_entries
+#   List of user-override entries. Used to override specific user data fields;
+#   UID, GID, GECOS, HOME_DIR and SHELL.
+#
+# @param user_override_hiera_merge
+#   FIXME Missing description
+#
+# @param group_override_entries
+#   List of group-override entries. Used to override specific group data fields;
+#   GROUP_NAME, GID and GROUP_MEMBERSHIP.
+#
+# @param group_override_hiera_merge
+#   FIXME Missing description
+#
+# @param username
+#   Name of user account used to join Active Directory.
+#
+# @param keytab_path
+#   The path to the keytab file used together with <username> to join Active Directory.
+#
+# @param keytab_source
+#   File source for the keytab file used to join Active Directory.
+#
+# @param keytab_owner
+#   keytab file owner.
+#
+# @param keytab_group
+#   keytab file group.
+#
+# @param keytab_mode
+#   keytab file mode.
+#
+# @param vas_fqdn
+#   FQDN to join to VAS as.
+#
+# @param computers_ou
+#   Path to OU where to store computer object.
+#
+# @param users_ou
+#   Deprecated, this parameter is the same as upm_search_path. Path to OU where
+#   to load UPM user profiles.
+#
+# @param nismaps_ou
+#   Path to OU where to load nismaps initially.
+#
+# @param nismaps_ou
+#   Path to OU where to load nismaps initially.
+#
+# @param user_search_path
+#   LDAP search path for user profiles. This will limit the scope where QAS will
+#   search for users when operating in RFC2307 mode.
+#
+# @param group_search_path
+#   LDAP search path for groups. This will limit the scope where QAS will search
+#   for groups when operating in RFC2307 mode.
+#
+# @param upm_search_path
+#   LDAP search path for UPM user profiles. Setting this parameter will cause
+#   QAS to run in UPM mode.
+#
+# @param nisdomainname
+#   Name of the NIS domain.
+#
+# @param realm
+#   Name of the realm.
+#
+# @param domain_change
+#   FIXME Missing description
+#
+# @param sitenameoverride
+#   Name of AD site to join. The AD site is determined automatically in AD by
+#   default.
+#
+# @param vas_conf_client_addrs
+#   client-addrs option in vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasypd_update_interval
+#   Integer for number of seconds vasypd will wait between checks for updated
+#   NIS Map information in Active Directory. See VAS.CONF(5).
+#
+# @param vas_conf_group_update_mode
+#   The value of group-update-mode in the [nss_vas] configuration section.
+#   This controls how directory searches will be handeled for group nss
+#   functions. See VAS.CONF(5) for more info.
+#   Possible values: force | force-if-missing | none
+#
+# @param vas_conf_root_update_mode
+#   The value of root-update-mode in the [nss_vas] configuration section.
+#   This controls how directory searches will be performed when calling nss
+#   functions. See VAS.CONF(5) for more info.
+#   Possible values: force | force-if-missing | none
+#
+# @param vas_conf_disabled_user_pwhash
+#   String to be used for disabled-user-pwhash option in vas.conf. If undef,
+#   line will be suppressed.
+#
+# @param vas_conf_expired_account_pwhash
+#   String to be used for expired-account-pwhash option in vas.conf.
+#   If undef, line will be suppressed.
+#
+# @param vas_conf_locked_out_pwhash
+#   String to be used for locked-out-pwhash option in vas.conf.
+#   If undef, line will be suppressed.
+#
+# @param vas_conf_preload_nested_memberships
+#   preload-nested-membership option in vas.conf. Set this to 'false' to speed
+#   up flush (and join) operations in VAS version 4.0.3-206 and later.
+#
+# @param vas_conf_update_process
+#   update-process option in vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_upm_computerou_attr
+#   upm-computerou-attr option in vas.conf. Changed to 'department' to work in
+#   a multi-AD-domain setup. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_update_interval
+#   Integer for number of seconds to set value of update-interval in [vasd]
+#   section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_auto_ticket_renew_interval
+#   Integer for number of seconds to set value of auto-ticket-renew-interval
+#   in [vasd] section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_lazy_cache_update_interval
+#   Integer for number of minutes for the value of lazy-cache-update-interval
+#   in [vasd] section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_timesync_interval
+#   Integer for number of seconds to set value of timesync-interval in
+#   [vasd] section of vas.conf. See VAS.CONF(5) for more info.
+#   If $::virtual is "zone" this value is set to 0
+#
+# @param vas_conf_vasd_cross_domain_user_groups_member_search
+#   Boolean to set value of cross-domain-user-groups-member-search in
+#   [vasd] section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_password_change_script
+#   Path for script to set value of password-change-script in
+#   [vasd] section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_password_change_script_timelimit
+#   Integer for number of seconds to set value of
+#   password-change-script-timelimit in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_workstation_mode
+#   Boolean to control whether or not vasd operates in Workstation mode.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_workstation_mode_users_preload
+#   Comma separated list of groups for preloading users in Workstation mode.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_workstation_mode_group_do_member
+#   Boolean to control if vasd should process group memberships in Workstation
+#   mode. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_workstation_mode_groups_skip_update
+#   Boolean that can be used to reduce the number of updates by vasd in
+#   Workstation mode. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_ws_resolve_uid
+#   Boolean to control whether vasd will resolve unknown UIDs when in
+#   Workstation mode. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_deluser_check_timelimit
+#   Integer for number of seconds to set value of deluser-check-timelimit in
+#   [vasd] section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_delusercheck_interval
+#   Integer for number of minutes to set value of delusercheck-interval in
+#   [vasd] section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_delusercheck_script
+#   Path for script to set value of delusercheck-script in [vasd] section of
+#   vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_username_attr_name
+#   String to be used for username-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_groupname_attr_name
+#   String to be used for groupname-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_uid_number_attr_name
+#   String to be used for uid-number-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_gid_number_attr_name
+#   String to be used for gid-number-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_gecos_attr_name
+#   String to be used for gecos-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_home_dir_attr_name
+#   String to be used for home-dir-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_login_shell_attr_name
+#   String to be used for login-shell-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_group_member_attr_name
+#   String to be used for group-member-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_memberof_attr_name
+#   String to be used for memberof-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_unix_password_attr_name
+#   String to be used for unix_password-attr-name  in [vasd] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vasd_netgroup_mode
+#   String to be used to set value of netgroup-mode in the [vasd] section of
+#   vas.conf. Valid values are 'NSS', 'NIS' and 'OFF'. If not specified, the
+#   netgroup-mode parameter will not be set in vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_prompt_vas_ad_pw
+#   prompt-vas-ad-pw option in vas.conf. Sets the password prompt for logins.
+#
+# @param vas_conf_pam_vas_prompt_ad_lockout_msg
+#   prompt-ad-lockout-msg option in vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_libdefaults_forwardable
+#   Boolean to set value of forwardable in [libdefaults] vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_libdefaults_tgs_default_enctypes
+#   FIXME Missing description
+#
+# @param vas_conf_libdefaults_tkt_default_enctypes
+#   FIXME Missing description
+#
+# @param vas_conf_libdefaults_default_etypes
+#   String to set value of default_etypes in [libdefaults] vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_libdefaults_default_cc_name
+#   String to set where kerberos cache files should be stored (default on most
+#   systems is /tmp/krb5cc_${uid}).
+#   Example: FILE:/new/path/krb5cc_${uid}
+#
+# @param vas_conf_vas_auth_uid_check_limit
+#   Integer for uid-check-limit option in vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_vas_auth_allow_disconnected_auth
+#   Boolean to set value of allow-disconnected-auth option in [vas_auth] section
+#   of vas.conf. See VAS.CONF(5) for more info. If set to 'UNSET' nothing will
+#   get printed.
+#
+# @param vas_conf_vas_auth_expand_ac_groups
+#   Boolean to set value of expand-ac-groups option in [vas_auth] section of
+#   vas.conf. See VAS.CONF(5) for more info. If set to 'UNSET' nothing will get
+#   printed.
+#
+# @param vas_conf_libvas_vascache_ipc_timeout
+#   Integer for number of seconds to set value of vascache-ipc-timeout in
+#   [libvas] section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_libvas_use_server_referrals
+#   Boolean to set valut of use-server-referrals in [libvas] section of vas.conf.
+#   See VAS.CONF(5) for more info. Set to 'USE_DEFAULTS' for automagically
+#   switching depending on running $vas_version.
+#   Also see $vas_conf_libvas_use_server_referrals_version_switch.
+#
+# @param vas_conf_libvas_use_server_referrals_version_switch
+#  String with version number to set use-server-referrals to false when
+#  $vas_conf_libvas_use_server_referrals is set to 'USE_DEFAULTS'.
+#  Equal or higher version numbers will pull the trigger.
+#
+# @param vas_conf_libvas_auth_helper_timeout
+#   Integer for number of seconds to set value of auth-helper-timeout in
+#   [libvas] section of vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_libvas_mscldap_timeout
+#   Integer to control the timeout when performing a MSCLDAP ping against
+#   AD Domain Controllers. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_libvas_site_only_servers
+#   Boolean to set valut of site-only-servers in [libvas] section of
+#   vas.conf. See VAS.CONF(5) for more info.
+#
+# @param vas_conf_libvas_use_dns_srv
+#   Boolean to set value of use-dns-srv in [libvas] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_libvas_use_tcp_only
+#   Boolean to set value of use-tcp-only in [libvas] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_lowercase_names
+#   Boolean to set value of lowercase-names in [nss_vas] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_conf_lowercase_homedirs
+#   Boolean to set value of lowercase-homedirs in [nss_vas] section of vas.conf.
+#   See VAS.CONF(5) for more info.
+#
+# @param vas_config_path
+#   Path to VAS config file.
+#
+# @param vas_config_owner
+#   vas.conf owner.
+#
+# @param vas_config_group
+#   vas.conf group.
+#
+# @param vas_config_mode
+#   vas.conf mode.
+#
+# @param vas_user_override_path
+#   Path to user-override file.
+#
+# @param vas_user_override_owner
+#   user-override file owner.
+#
+# @param vas_user_override_group
+#   user-override file group.
+#
+# @param vas_user_override_mode
+#   user-override file mode.
+#
+# @param vas_group_override_path
+#   Path to user-override file.
+#
+# @param vas_group_override_owner
+#   group-override file owner.
+#
+# @param vas_group_override_group
+#   group-override file group.
+#
+# @param vas_group_override_mode
+#   group-override file mode.
+#
+# @param vas_users_allow_path
+#   Path to users.allow file.
+#
+# @param vas_users_allow_owner
+#   users.allow file owner.
+#
+# @param vas_users_allow_group
+#   users.allow file group.
+#
+# @param vas_users_allow_mode
+#   users.allow file mode.
+#
+# @param vas_users_deny_path
+#   Path to users.deny file.
+#
+# @param vas_users_deny_owner
+#   users.deny file owner.
+#
+# @param vas_users_deny_group
+#   users.deny file group.
+#
+# @param vas_users_deny_mode
+#   users.deny file mode.
+#
+# @param vasjoin_logfile
+#   Path to logfile used by AD join commando.
+#
+# @param vastool_binary
+#   Path to vastool binary to create symlink from.
+#
+# @param symlink_vastool_binary_target
+#   Path to where the symlink should be created.
+#
+# @param symlink_vastool_binary
+#   Boolean for ensuring a symlink for vastool_binary to
+#   symlink_vastool_binary_target. This is useful since /opt/quest/bin is a
+#   non-standard location that is not in your $PATH.
+#
+# @param license_files
+#   Hash of license files.
+#
+# @param domain_realms
+#   Hash of domains that should be mapped to correct realm.
+#
+# @param join_domain_controllers
+#   A string or an array with domain controllers to contact during the join
+#   process. Normally the servers for the domain will be automatically detected
+#   through DNS and LDAP lookups. By specifying this parameter vastool will
+#   contact the specified servers and only those servers during the join process.
+#   This can be useful if the machine being joined is not able to talk with all
+#   global Domain Controllers (e.g. due to firewalls). Note that this will have
+#   no effect after the join, where normal site discovery of servers will be
+#   made.
+#
+# @param unjoin_vas
+#   Boolean to trigger an unjoining of the domain. Obviously this will only
+#   work if the system is joined to a domain.
+#
+# @param use_srv_infocache
+#   A bool to achieve the same thing as issuing "vastool configure vas libvas
+#   use-srv-info-cache <bool>" Only has any effect if set to false.
+#
+# @param kdcs
+#   An array of kdcs that are to be entered under the [realms] section. If set
+#   has the same effect as issuing
+#   "vastool configure realm domain.tld srv1.domain.tld srv2.domain.tld". (eg)
+#
+# @param kdc_port
+#   An integer containing the kdc port. Has no effect unless kdcs is populated
+#   with servernames.
+#
+# @param kpasswd_servers
+#   An array of kpasswd servers that are to be entered under the [realms] section
+#   Normally needs not be set unless you want something different than the value
+#   of kdcs (above).
+#
+# @param kpasswd_server_port
+#   An integer containing the kpasswd server port. Has no effect unless
+#   kpasswd_servers or kdcs is populated with servernames.
+#
+# @param api_enable
+#   A boolean to control, whether the API function is called. If called, the API
+#   will return a list of entries for the users.allow file. This result will be
+#   merged with whatever content is provided otherwise provided; i.e. it will be
+#   concatenated with the content created by parameters users_allow_entries and
+#   users_allow_hiera_merge.
+#
+# @param api_users_allow_url
+#   The URL towards the API.
+#
+# @param api_token
+#   Security token for authenticated access to the API.
 #
 class vas (
   $manage_nis                                           = true,
